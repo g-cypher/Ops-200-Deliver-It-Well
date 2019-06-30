eda0049f436f1d3cb79950c0816b7dc02647caa8
Garrett <cyph17@gmail.com>
2019-06-30 14:21:23 -0700

    first circle CI push

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,14 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page says Jesus take the wheel', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('Jesus take the wheel');
+        done();
+      });
+    });
 })
